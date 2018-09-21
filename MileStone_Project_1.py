947b3d5a731e11e735a7a556a1284fb978b0e927
Alex <marchenko.alexander@gmail.com>
2018-09-21 13:22:51 +0200

    routine update + add new file

---
+++ MileStone_Project_1.py
@@ -0,0 +1,11 @@
+"""
+Assignment: Create a Tic Tac Toe game.
+
+Here are the requirements:
+2 players should be able to play the game (both sitting at the same computer)
+The board should be printed out every time a player makes a move
+Programme should be able to accept input of the player position and then place a
+symbol on the board.
+
+"""
+
