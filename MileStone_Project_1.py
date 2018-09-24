b3eed9b3c05a625462a3939023cbd03ca346a58c
Alex <marchenko.alexander@gmail.com>
2018-09-24 15:51:21 +0200

    routine update

--- MileStone_Project_1.py
+++ MileStone_Project_1.py
@@ -1,17 +1,50 @@
 """
 Task: Create a Tic Tac Toe game.
 
-Here are the requirements:
+Requirements:
 2 players should be able to play the game (both sitting at the same computer)
 The board should be printed out every time a player makes a move
-Programme should be able to accept input of the player position and then place a
-symbol on the board.
+Programme should be able to ask for an input of the player position, accept it
+and then place a symbol on the board. Programme should be able to check if
+someone has won, too, announce the winner and offer replay.
 
 """
 
-# Collect names of the players
+
+def display_board(board):
+    """Displays the 'board' with marks ('X' and 'O'), assigned to positions"""
+    pass
+
+
+def space_check(board):
+    """Checks if the position chosen is empty/free and tells to replay if it is not"""
+    pass
+
+
+def player_input():
+    """Ask player for position (1 through 9), where he/she wants to play"""
+    pass
+
+
+def mark_position(board, mark, position):
+    """Assign 'X' or 'O' (whichever the player plays to specified/chosen position"""
+    pass
+
+
+def win_ceck(board, mark):
+    """ Check if one of the players has won"""
+    pass
+
+
+def replay():
+    """Ask if the players want to play again, start a new game if yes, end if not"""
+    pass
+
+
+# Start the Game
 print("Welcome to Tic Tak Toe!")
 
+# Collect names of the players
 player1 = input("\nPlayer 1, your name please: ")
 player2 = input("\nPlayer 2, your name please: ")
 
@@ -25,31 +58,3 @@ else:
     p2 = "X"
 
 print(f"\nIn this case, {player2}, you will be playing '{p2}'.")
-
-
-def display_board(board):
-
-    pass
-
-
-def space_check(board):
-
-    pass
-
-
-def player_input():
-
-    pass
-
-
-def place_marker(board, marker, position):
-
-    pass
-
-
-def win_ceck(board, mark):
-
-    pass
-
-
-
