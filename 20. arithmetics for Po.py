f850116e069804b079dbe57f238cd4737f90223b
maralexv <43134894+maralexv@users.noreply.github.com>
2023-08-18 10:27:15 +0200

    update arithmetics with csv file writing

--- 20. arithmetics for Po.py
+++ 20. arithmetics for Po.py
@@ -8,7 +8,7 @@ a, b, r, x = 0, 0, 0, 0
 operator = ''
 rows = []
 
-while x <6:
+while x <50:
 
     a = random.choice(int_list)
     b = random.choice(int_list)
@@ -19,16 +19,25 @@ while x <6:
 
     if operator == 'add':
         r = a + b
-        rows.append([a, ' + ', b, ' = ', ' ___'])
+        rows.append([a, '+', b, '='])
     elif a >= b:
         r = a + b 
-        rows.append([r, ' - ', b, ' = ', ' ___'])
+        rows.append([r, '-', b, '='])
     else:
         r = b + a
-        rows.append([r, ' - ', a, ' = ', ' ___'])
+        rows.append([r, '-', a, '='])
 
     x = x + 1
 
-for el in rows:
-    print (el)
+# define scv file
+file = 'Po-arithmetics.csv' 
+
+# create and/or open csv file for writing
+with open(file, 'w') as f:
+
+    # create csv writer object
+    csvwriter = csv.writer(f)
+
+    # write rows into csv file
+    csvwriter.writerows(rows)
 
