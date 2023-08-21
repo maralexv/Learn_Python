a176044c5ff55252cf76652e9ade235d224d0cce
maralexv <43134894+maralexv@users.noreply.github.com>
2023-08-21 18:12:19 +0200

    minor update

--- Po_arithmetics/arithmetics_problems.py
+++ Po_arithmetics/arithmetics_problems.py
@@ -4,30 +4,34 @@ import csv
 int_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 ops_list = ['add', 'sub']
 
-a, b, r, x = 0, 0, 0, 0
+a, b, r, i = 0, 0, 0, 0
 operator = ''
 rows = []
 
-while x <50:
+while i <50:
 
     a = random.choice(int_list)
+
     b = random.choice(int_list)
+
     operator = random.choice(ops_list)
 
-    if a + b < 11:
+
+    if a + b < 10:
         continue
 
     if operator == 'add':
-        r = a + b
         rows.append([a, '+', b, '='])
-    elif a >= b:
-        r = a + b 
-        rows.append([r, '-', b, '='])
     else:
         r = b + a
         rows.append([r, '-', a, '='])
 
-    x = x + 1
+    i = i + 1
+
+"""
+for el in rows:
+    print (el)
+"""
 
 # define scv file
 file = 'Po-arithmetics.csv' 
